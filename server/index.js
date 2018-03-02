c17d6bff3f0efab715f6c031172d98f6d6913288
anthony burt <anthonyburt@gmail.com>
2018-03-01 21:31:53 -0500

    fixed graph request

--- server/index.js
+++ server/index.js
@@ -1,12 +1,9 @@
-import GraphQLClient  from 'graphql-request';
+import request from 'graphql-request';
 import test from './test';
 
 test('PRODUCTION IS STILL WORKING!!!');
 
-// use this instead for cookie support
-global['fetch'] = require('fetch-cookie/node-fetch')(require('node-fetch'))
-
-const client = new GraphQLClient('https://api.graph.cool/simple/v1/cjdz30buh2px90159fuptggd5')
+const end_point = 'https://api.graph.cool/simple/v1/cjdz30buh2px90159fuptggd5'
 
 const query = `{
     allTestResultses(orderBy: createdAt_DESC) {
@@ -21,4 +18,4 @@ const query = `{
   }
 }`
 
-client.request(query).then(data => console.log(data))
+request(end_point, query).then(data => console.log(data))
