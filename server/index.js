aecc9e57b3ddf73be5a17e830286501b2ae63f70
anthony burt <anthonyburt@gmail.com>
2018-03-01 21:21:28 -0500

    fix endpoint and query

--- server/index.js
+++ server/index.js
@@ -6,14 +6,18 @@ test('PRODUCTION IS STILL WORKING!!!');
 // use this instead for cookie support
 global['fetch'] = require('fetch-cookie/node-fetch')(require('node-fetch'))
 
-const client = new GraphQLClient('my-endpoint')
+const client = new GraphQLClient('https://api.graph.cool/simple/v1/cjdz30buh2px90159fuptggd5')
 
 const query = `{
-  Movie(title: "Inception") {
-    releaseDate
-    actors {
-      name
-    }
+    allTestResultses(orderBy: createdAt_DESC) {
+        id
+        createdAt
+        products {name}
+        os
+        browser
+        testname
+        buildNumber
+        testStatus
   }
 }`
 
