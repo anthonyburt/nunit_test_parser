fe0a40a71d10f7f80172270d3e1e1257293aa8c1
anthony burt <anthonyburt@gmail.com>
2018-03-01 22:13:12 -0500

    reading in xml file from disk, and parsing a specifc node

--- server/index.js
+++ server/index.js
@@ -1,7 +1,4 @@
 import request from 'graphql-request';
-import test from './test';
-
-test('PRODUCTION IS STILL WORKING!!!');
 
 const end_point = 'https://api.graph.cool/simple/v1/cjdz30buh2px90159fuptggd5'
 
@@ -18,4 +15,28 @@ const query = `{
   }
 }`
 
-request(end_point, query).then(data => console.log(data))
+
+// example of connecting to graphcool
+request(end_point, query).then(data => console.log(data));
+
+// example of reading test_results.xml file then outputting a specific node from the xml file
+var xml2js = require('xml2js');
+var xml;
+
+var fs = require('fs'),
+    path = require('path'),    
+    filePath = path.join(__dirname, 'test_results.xml');
+var extractedData = '';
+
+fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+    if (!err) {
+            var parser = new xml2js.Parser();
+            parser.parseString(data, function(err,result){
+                 var extractedData = result['test-run']['passed'];
+                 console.log(extractedData);
+            });
+    } else {
+        console.log(err);
+    }
+});
+
